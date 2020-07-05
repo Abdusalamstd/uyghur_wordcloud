02068f2a4d1421ce7cf2fc2604dafc4c983f8c50
Abduroid <50683433+Abdusalamstd@users.noreply.github.com>
2020-07-05 23:58:16 +0800

    Update soz.py

--- soz.py
+++ soz.py
@@ -3,7 +3,7 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt 
 from bidi.algorithm import get_display
 import arabic_reshaper
-path_txt='a.txt'
+path_txt='abc.txt'
 f = open(path_txt,'r',encoding='UTF-8').read()
 reshaped_text = arabic_reshaper.reshape(f)
 f = get_display(reshaped_text)
